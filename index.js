c4254f6a81cd0f7a34cfd0c97c31867075438c00
Amit Bhushan <imamit.a001@gmail.com>
2022-11-14 00:48:46 +0530

    Started JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const title= document.getElementById("title");
+const description= document.getElementById("description");
+const form= document.querySelector("form");
+const container= document.querySelector(".container");
+
+const tasks = [];
