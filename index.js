e5ff83522c739f3ead31ad3c66b39cfd2da4518a
Amit Bhushan <imamit.a001@gmail.com>
2022-11-15 21:35:35 +0530

    Update JS

--- index.js
+++ index.js
@@ -4,3 +4,36 @@ const form= document.querySelector("form");
 const container= document.querySelector(".container");
 
 const tasks = [];
+
+function showAllTasks () {
+    tasks.forEach ((value,index)=> {
+        const div = document.createElement("div");
+        div.setAttribute("class", "task");
+        const innerDiv = document.createElement("div");
+        div.append(innerDiv);
+
+        const p = document.createElement("p");
+        p.innerText=value.title;
+        innerDiv.append(p);
+
+        const span = document.createElement("span");
+        span.innerText=value.description;
+        innerDiv.append(span);
+
+        const btn = document.createElement("btn");
+        btn.setAttribute("class", "deleteBtn");
+        btn.innerText="-";
+        div.append(btn);
+        container.append(div);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    tasks.push({
+        title:title.value,
+        description: description.value,
+    });
+    console.log(tasks);
+    showAllTasks();
+});
